7dd4f31b532a13879c4ae08185819cb87c21ac46
David Andrés Luna Tenecela <136658235+DavidlunaT@users.noreply.github.com>
2025-01-15 18:42:43 +0000

    achu

--- backend/settings.py
+++ backend/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+# Importe los módulos firebase_admin y credentials
+import firebase_admin
+from firebase_admin import credentials
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +41,8 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "main",
     "django.contrib.staticfiles",
+    'rest_framework',
+    'api',
 ]
 
 MIDDLEWARE = [
@@ -126,3 +131,10 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+# Coloque la ruta relativa al archivo con la clave privada
+FIREBASE_CRED = credentials.Certificate("keys/landing-key.json")
+
+# Inicialice la conexión con el Realtime Database con la clave privada y la URL de referencia
+firebase_admin.initialize_app(FIREBASE_CRED, {
+    'databaseURL': 'https://landing-eaf66-default-rtdb.firebaseio.com/'
+})
